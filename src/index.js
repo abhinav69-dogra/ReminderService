e736ecb1eff8010cf2219e033b1ce9c346f46c66
Abhinav Dogra <65779864+abhinav69-dogra@users.noreply.github.com>
2024-08-04 16:29:34 +0530

    message queues finished

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const EmailService = require('./services/email-service');
 const TicketController = require('./controllers/ticket-controller');
 
 const { PORT } = require('./config/serverConfig');
 
 // const { sendBasicEmail } = require('./services/email-service');
 const jobs = require('./utils/job')
+const { subscribeMessage , createChannel} = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 
-
-const setupAndStartServer = () => {
+const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
     app.post('/api/v1/tickets', TicketController.create);
 
+    const channel = await createChannel();
+    subscribeMessage(channel,EmailService.subscribeEvents,REMINDER_BINDING_KEY)
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
         jobs();
